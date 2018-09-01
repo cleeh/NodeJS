8b2b123c1bc704da39b223821bb65ac18aa964e4
rockatoo <youlia73@naver.com>
2018-09-01 23:35:51 +0900

    Javascript: Function and Object - Overlapping Function Context

--- javascript/function and object - context.js
+++ javascript/function and object - context.js
@@ -1,12 +1,14 @@
 function doA(){
 	var sum = 0;
+	var doB = function(){
+		console.log("sum:", sum);
+		console.log("this.sum:", this.sum); // 'this' is context
+	};
 	
-	console.log("sum:", sum);
-	console.log("this.sum:", this.sum); // 'this' is context
+	doB();
 }
 
 var sum = 100;
 var obj = {sum: 10000};
 
-doA(); console.log("------------------------------");
 doA.apply(obj);
