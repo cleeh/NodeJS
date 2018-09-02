55f3aaeb7f45620f5d8adafee38dc3fcf9ba8243
rockatoo <youlia73@naver.com>
2018-09-02 14:38:07 +0900

    Javascript: Function and Obejct - Prototype

--- javascript/function and object - object literal and prototype.js
+++ javascript/function and object - object literal and prototype.js
@@ -5,3 +5,29 @@ console.dir(obj);
 console.dir('v1: ' + obj['v1']);
 console.dir('v2: ' + obj['v2']);
 console.dir('v3: ' + obj.v3);
+
+function Person(ssn){
+	this.ssn = ssn;
+
+	Person.prototype.display_in = function(){
+		console.log('[' + this.ssn + '] display_in...');
+	};
+}
+
+Person.prototype.display_out = function(){
+	console.log('[' + this.ssn + '] display_out...');
+};
+
+// it looks like 'class'
+var p1 = new Person('Apple');
+var p2 = new Person('Banana');
+var p3 = new Person('Carrot');
+
+console.dir(p1);
+console.dir(p2);
+console.dir(p3);
+console.dir(Person);
+
+p1.display_in();
+p2.display_out();
+p3.display_out();
