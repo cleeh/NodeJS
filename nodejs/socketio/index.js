5f843020515278a519b66235818311d8f1e814a5
rockatoo <youlia73@naver.com>
2018-09-06 18:51:38 +0900

    Node.js: Socket.io Module - Broadcasting

--- nodejs/socketio/index.js
+++ nodejs/socketio/index.js
@@ -6,6 +6,18 @@ app.get('/',, function(req, res){
 	res.sendFile(__dirname + '/index2.html');
 });
 
+io.on('connection', function(socket){
+//	console.log('a user connected');
+//	socket.on('disconnect', function(){
+//		console.log('user disconnected');
+//	});
+
+	socket.on('caht message', function(msg){
+//		console.log('message: ' + msg);
+		io.emit('chat message', msg); // Broadcasting
+	});
+});
+
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
