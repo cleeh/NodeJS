d96bd53401ac967efec618165e564164cb3c29bd
rockatoo <youlia73@naver.com>
2018-09-08 14:18:03 +0900

    Node.js: Node Cluster - PM2

---
+++ nodejs/cluster/pm2.js
@@ -0,0 +1,7 @@
+const http = require('http');
+// Workers can share any TCP connection
+// In this case it is an HTTP server
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8000);
